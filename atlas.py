65510ac1a5aab8dec218a6b362b16599b456ce44
jackgladowsky <jackgladowsky@gmail.com>
2025-01-22 22:32:03 -0500

    added shell script to activate venv and run

--- atlas.py
+++ atlas.py
@@ -2,8 +2,11 @@ from atlas.model import ChatModel, ChatRequest, ChatResponse
 
 def main():
     # Create a chat model
-    model = ChatModel(model="hermes-3-llama-3.2-3b")
+    # model = ChatModel(model="hermes-3-llama-3.2-3b")
     # model = ChatModel(model="qwen2-vl-7b-instruct")
+    model = ChatModel(model="granite-3.1-8b-instruct")
+
+    
 
     # Run the base chat model
     model.run()
---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# start the venv
+source .venv/bin/activate
+
+# run the program with uv
+uv run atlas.py
